a7e4753b9d74e9285c154446a0b3fd0bf2bccb19
aishwy <aishi.best@gmail.com>
2018-12-14 22:40:58 +0530

    create-react-app

---
+++ libraryx/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import header from './UI/header/header';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header/>
+      </div>
+    );
+  }
+}
+
+export default App;
